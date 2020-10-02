1fe2962067be7525f9c504a868cd8cad31ad7e39
Jon Crowell <jcrowell@hksinc.com>
2020-10-02 10:14:00 -0500

    Add presets to webpack config to fix babel errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 const App = () => (
+  <div>
+    <h1>Hotload this sucka</h1>
     <div>Hello form Webpack!</div>
-)
+  </div>
+);
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
